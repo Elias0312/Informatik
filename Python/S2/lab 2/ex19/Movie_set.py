05fdb0f18c52193ea9258a8a50affacff37bb2d8
Elias0312 <en026@hdm-stuttgart>
2026-06-21 15:58:18 +0200

    rest ex 19

--- Python/S2/lab 2/ex19/Movie_set.py
+++ Python/S2/lab 2/ex19/Movie_set.py
@@ -39,4 +39,43 @@ class Task:
             status = "incomplete"
         return f"{self.description} -- assigned to: {self.assigned_to.name}, status: {status}"
     
+class Project:
+    def __init__(self, title):
+        self.title = title
+        self.members = []
+        self.assets = []
+        self.tasks = []
 
+    def add_member(self, person):
+        self.members.append(person)
+        return
+
+    def add_asset(self, media_asset):
+        self.assets.append(media_asset)
+        return
+    
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def progress(self):
+        status = 0
+        for i in self.tasks:
+            if i.completed == True:
+                status += 1
+            
+        prog = status/len(self.tasks)
+
+        return f"{prog*100}%"
+    
+    def summary(self):
+        print(self.title)
+        print("Members:")
+        for i in self.members:
+            print(i.name)
+        print("\nAssets:")
+        for i in self.assets:
+            print(i.title)
+        print("\nTasks:")
+        for i in self.tasks:
+            print(i.description)
+        print(f"\nProgress: {self.progress()}")
